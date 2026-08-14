23deb48383fbc976ed394e85a324adf587cfde61
its-aastha <valandastha@gmail.com>
2026-08-14 11:39:47 +0530

    #2 Chormadb added

---
+++ chromadb/query.py
@@ -0,0 +1,29 @@
+import chromadb 
+
+client = chromadb.Client()  # type: ignore
+
+collection = client.create_collection(name = "Vehicles")
+print("collection Created:-",collection.name)
+
+#Add the data to the collection
+collection.add(
+    #Documents == Your data 
+    documents = [
+        "cars runs on the land",
+        "Plane flies in the sky",
+        "boat travels on the water",
+        "bus is public transport on the road"
+    ],
+    ids = [
+        "car1","plane1","boat1","bus1"
+    ]
+)
+
+#Query the collection 
+results = collection.query(
+    #Query to run eg :
+    #1. vehicles that on the roads
+query_texts = ["i have to cath the fish , what should i use "],
+n_results = 2
+)
+print(results)
