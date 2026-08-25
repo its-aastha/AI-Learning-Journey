21bbbdea302af7b0d2ca35950f8192c35eac2bf9
its-aastha <valandastha@gmail.com>
2026-08-25 15:13:15 +0530

    #7 Handle the data collection

--- chromadb/create_collection.py
+++ chromadb/create_collection.py
@@ -1,12 +1,23 @@
 import chromadb
 
-#client = chromadb.Client() # type: ignore
-client = chromadb.PersistentClient(path = "./chorma_db") # type: ignore
+client = chromadb.PersistentClient() # type: ignore
+collection  = client.get_or_create_collection(name = "Vehicle")
+print("collection created :- ", collection.name)
 
-collection = client.create_collection("Vehicle")
-
-collection.add(
-    documents = ["Car runs on the road"],
-    ids = ["cars1"]
+collection.add (
+    documents = [
+        "Car runs on road",
+        "Plane flies in the sky",
+        "boat travels on water",
+        "bus is public transport on road"],
+    ids = [
+        "cars1","plane1","boat1","bus1"
+    ]
 )
-print("data added and saved a permanenatly ")
+print("Added the Successfully !")
+
+data = collection.get() 
+
+#This is the code that is used to display the data 
+for i, doc in zip(data["ids"], data["documents"]):
+    print(f"{i} -> {doc}")
