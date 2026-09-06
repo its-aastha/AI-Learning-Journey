b643226dafd9235e2343003634b860475dc384e1
its-aastha <valandastha@gmail.com>
2026-09-06 18:44:47 +0530

    #15 view_embedding update(list of the vectors)

--- chromadb/embedding_automation/view_embeddings.py
+++ chromadb/embedding_automation/view_embeddings.py
@@ -6,6 +6,6 @@ collection = client.get_collection(name = "vehicles_embeddings")
 
 data = collection.get(include=["documents","embeddings"])
 
-for doc_id,text,emed in zip(data["documents"],data["embeddings"],data["ids"]): #type: ignore
+for doc_id,text,emed in zip(data["ids"],data["documents"],data["embeddings"]): #type: ignore
     print(f"\n {doc_id}:{text}")
     print(f"Embedding (First 10 values): {emed[:10]}") #show first few numbers only
